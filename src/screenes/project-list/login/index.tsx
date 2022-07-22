4c7f53c0f2d04fa2d5dc18224780c1dc8c6feb63
SouthWindss1 <101077221+SouthWindss1@users.noreply.github.com>
2022-07-22 11:50:52 +0800

    feat:添加登陆页

---
+++ src/screenes/project-list/login/index.tsx
@@ -0,0 +1,22 @@
+const LoginPage =()=>{ 
+const handleSubmit = (event:FormEvent<Element>)=>{
+    event.preventDefault()
+  const username = (event.currentTarget.elements[0] as HTMLInputElement).value;
+  const password = (event.currentTarget.elements[1] as HTMLInputElement).value
+}
+
+ return(<>
+ <form submit={handleSubmit}>
+    <div>
+        <label htmlFor="username">用户名</label>
+        <input type="text" id={'username'}/>
+    </div>
+    <div>
+        <label htmlFor="password">密码</label>
+        <input type="password"  id={'password'}/>
+    </div>
+    <button type={'submit'}>登陆</button>
+ </form>
+ </>)
+}
+export default LoginPage;
