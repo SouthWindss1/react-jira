ae55860224153109998f48034f67bb7440d96c0b
南风zzz <942284005@qq.com>
2022-07-24 17:55:50 +0800

    feat:完善样式

--- src/Authenticated-app.tsx
+++ src/Authenticated-app.tsx
@@ -1,11 +1,69 @@
 import React from "react";
 import { ListPage } from "./screenes/project-list";
 import { useAuth } from "./context/auth-context";
-const AuthentiCated = () => {
-    const authValue = useAuth()
-    return (<>
-        <button onClick={authValue?.logout}>登出</button>
-        <ListPage />
-    </>)
+import { ReactComponent as SoftwareLogo } from "./assets/software-logo.svg";
+import styled from "@emotion/styled";
+import { Row } from "./compoents/lib";
+import { Dropdown, Menu, Button } from 'antd'
+export function AuthentiCated() {
+    return (
+        <Container>
+            <PageHeader />
+            <Main></Main>
+            <ListPage />
+        </Container>
+    );
 }
+
+const PageHeader = () => {
+    return (
+        <Header between={true}>
+            <HeaderLeft gap={true}>
+                <SoftwareLogo width={"18rem"} color={"rgb(38, 132, 255)"} />
+            </HeaderLeft>
+            <HeaderRight>
+                <User />
+            </HeaderRight>
+        </Header>
+    );
+};
+
+const User = () => {
+    const authValue = useAuth()
+    return (
+        <Dropdown
+            overlay={
+                <Menu>
+                    <Menu.Item key={"logout"}>
+                        <Button onClick={authValue?.logout} type={"link"}>
+                            登出
+                        </Button>
+                    </Menu.Item>
+                </Menu>
+            }
+        >
+            <Button type={"link"} onClick={(e) => e.preventDefault()}>
+                Hi, {authValue?.user?.name}
+            </Button>
+        </Dropdown>
+    );
+};
 export default AuthentiCated;
+const Container = styled.div`
+  /* display: grid; */
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+// grid-area 用来给grid子元素起名字
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
+`;
+const HeaderLeft = styled(Row)``;
+const HeaderRight = styled.div``;
+const Main = styled.main`
+  display: flex;
+  overflow: hidden;
+`;
--- src/screenes/project-list/list.tsx
+++ src/screenes/project-list/list.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import React from "react";
 import { Table } from 'antd'
+import dayjs from "dayjs";
 import { ListProps } from "./type";
 export const List: FC<ListProps> = (props) => {
     const { list, users } = props;
@@ -11,13 +12,29 @@ export const List: FC<ListProps> = (props) => {
                 dataIndex: "name"
             },
             {
+                title: "部门",
+                dataIndex: "organization",
+            },
+            {
                 title: "负责人",
-                render(value:any, record:any,) {
+                render(value: any, record: any,) {
                     return <span>
                         {users.find(res => res.id === record.personId)?.name || '未知'}
                     </span>
                 },
-            }
+            },
+            {
+                title: "创建时间",
+                render(value: any, project: any) {
+                    return (
+                        <span>
+                            {project.created
+                                ? dayjs(project.created).format("YYYY-MM-DD")
+                                : "无"}
+                        </span>
+                    );
+                },
+            },
         ]
     return (
         <>
